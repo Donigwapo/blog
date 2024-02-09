7fc38ad74adce8d773d7dbf28704e1502ea9db17
Donni Rhey Noval <donnirhey4blog@gmail.com>
2024-02-10 01:54:17 +0800

    When commit it will add important update

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,4 +1,3 @@
 class Article < ApplicationRecord
-    validates :title, presence: true, length: { minimum: 5, maximum: 100 }
-    validates :body, presence: true, length: { minimum: 10 }
-  end
+    validates(:title, presence: true)
+end
